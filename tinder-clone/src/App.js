6a13e3117fa07c7c8c98b7f8bf757e27fafb7f9d
phoenixx1 <xphoenix1010@gmail.com>
2021-04-16 21:40:49 +0530

    tinder-clone

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {/* Tider Cards */}
+      {/* Swipe Button */}
+    </div>
+  );
+}
+
+export default App;
