a2812ecdcef2d985f834108149587050a58592b8
phoenixx1 <xphoenix1010@gmail.com>
2021-04-18 18:14:03 +0530

    tinder-backend

--- tinder-clone/tinder-backend/server.js
+++ tinder-clone/tinder-backend/server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Cards = require("./dbCards");
+const Cors = require("cors");
+const connectionURL = require("./connection_url");
 
 // App Config
 const app = express();
 const port = process.env.PORT || 8001;
-const connection_url =
-  "mongodb+srv://admin:6KlnApDp3m2XVXLf@cluster0.y1gi9.mongodb.net/tinderdb?retryWrites=true&w=majority";
+// get your own connection url from mongodb website creating a new app.
+const connection_url = connectionURL;
 
 // Middlewares
+app.use(express.json());
+app.use(Cors());
 
 // DB config
 mongoose.connect(connection_url, {
@@ -20,19 +24,19 @@ mongoose.connect(connection_url, {
 // API Endpoints
 app.get("/", (req, res) => res.status(200).send("Hello Friend"));
 
-app.post("/tinder/card", (req, res) => {
+app.post("/tinder/cards", (req, res) => {
   const dbCard = req.body;
 
   Cards.create(dbCard, (err, data) => {
     if (err) {
       res.status(500).send(err);
     } else {
-      res.status(200).send(data);
+      res.status(201).send(data);
     }
   });
 });
 
-app.get("/tinder/card", (req, res) => {
+app.get("/tinder/cards", (req, res) => {
   Cards.find((err, data) => {
     if (err) {
       res.status(500).send(err);
--- tinder-clone/tinder-clone/src/components/TinderCards.js
+++ tinder-clone/tinder-clone/src/components/TinderCards.js
@@ -1,19 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TinderCard from "react-tinder-card";
 import styled from "styled-components";
+import axios from "./axios";
 
 function TinderCards() {
-  const [people, setPeople] = useState([
-    {
-      name: "Jeff Bezos",
-      url:
-        "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5bb22ae84bbe6f67d2e82e05%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D560%26cropX2%3D1783%26cropY1%3D231%26cropY2%3D1455",
-    },
-    {
-      name: "Satya Nadella",
-      url: "https://img.caixin.com/2018-11-24/1543035822545341.jpg",
-    },
-  ]);
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get("/tinder/cards");
+      setPeople(req.data);
+    }
+    fetchData();
+  }, []);
 
   const swiped = (direction, nameToDelete) => {
     console.log("removing: ", +nameToDelete);
@@ -34,7 +33,7 @@ function TinderCards() {
             onSwipe={(dir) => swiped(dir, person.name)}
             onCardLeftScreen={() => outOfFrame(person.name)}
           >
-            <Card style={{ backgroundImage: `url(${person.url})` }}>
+            <Card style={{ backgroundImage: `url(${person.imgUrl})` }}>
               <CardContent>
                 <h3>{person.name}</h3>
               </CardContent>
